1dae289a3ad175a732955b4821b88cc9bbdab169
ckatieng <katie.cy.ng@gmail.com>
2023-08-26 20:18:53 -0700

    remove navigation from loading screen

---
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,11 @@
+import './LoadingScreen.scss';
+
+function LoadingScreen () {
+    return (
+        <div className="loading">
+            <h2 className="loading__title">Loading...</h2>
+        </div>
+    );
+}
+
+export default LoadingScreen;
