4a1c1ed3ba8c44057e3b575f27bbb23b6dee0a69
ckatieng <katie.cy.ng@gmail.com>
2023-09-02 16:10:04 -0700

    add empty and loading states

--- src/pages/FavouritesPage/FavouritesPage.jsx
+++ src/pages/FavouritesPage/FavouritesPage.jsx
@@ -5,29 +5,45 @@ import "./FavouritesPage.scss";
 function FavouritesPage () {
     // States
     const [favouriteItems, setFavouriteItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Favourites API URL
+    const favouritesURL = "http://localhost:5050/visited";
 
     // GET request
     useEffect(() => {
         // GET array of all favourite items
-        axios.get("http://localhost:5050/favourites")
+        axios.get(favouritesURL)
             .then((response) => {
                 setFavouriteItems(response.data);
+                setIsLoading(false);
             })
             .catch((error) => {
                 console.error("Error fetching favourite items:", error);
+                setIsLoading(false);
             });
     }, []);
 
     return (
         <div className="favourites">
             <h2 className="favourites__title">Favourites List</h2>
-            <ul className="favourites__list">
-                {favouriteItems.map((item) => (
-                    <li key ={item.id}>
-                        {item.google_places_id}
-                    </li>
-                ))}
-            </ul>
+            {isLoading ? (
+                // Display a loading message while fetching data
+                <p>Loading...</p> 
+            ) : (
+                <ul className="favourites__list">
+                    {favouriteItems.length === 0 ? (
+                        // Display an empty state message when the list is empty
+                        <p>No items in the favourites list.</p> 
+                    ) : (
+                        favouriteItems.map((item) => (
+                            <li key={item.id}>
+                                {item.google_places_id}
+                            </li>
+                        ))
+                    )}
+                </ul>
+            )}
         </div>
     );
 }
