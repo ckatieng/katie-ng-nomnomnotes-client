f71c580821b74c703b986a161df1746445351df0
ckatieng <katie.cy.ng@gmail.com>
2023-09-18 11:52:53 -0700

    add restaurant photos and reviews

--- src/components/RestaurantDetails/RestaurantDetails.jsx
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -10,6 +10,8 @@ import AddIcon from '@mui/icons-material/Add';
 import Snackbar from '@mui/material/Snackbar';
 import Slide from '@mui/material/Slide';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 /*
  * RestaurantDetails Component
@@ -85,45 +87,89 @@ function RestaurantDetails() {
     return (
         <div className="restaurant-details">
             <div className="restaurant-details__container">
-            <IconButton
-                aria-label="close"
-                onClick={handleCloseClick}
-                sx={{
-                    position: 'absolute',
-                    right: 8,
-                    top: 8,
-                    color: (theme) => theme.palette.grey[500],
-                }}
-            >
-                <CloseIcon />
-            </IconButton>
-            <div className="restaurant-details__restaurant">
-                <h2 className="restaurant-details__name">{restaurantDetails.name}</h2>
-                <div className="restaurant-details__add">
-                    <IconButton disableTouchRipple className="restaurant-details__add-icon" size="medium" onClick={() => addItemHandler(placeId)} style={{ color:'#73649b' }}>
-                        <AddIcon fontSize="inherit"/>
-                    </IconButton>
-                    <Snackbar
-                        open={snackbarOpen}
-                        autoHideDuration={2000} 
-                        onClose={() => setSnackbarOpen(false)}
-                        TransitionComponent={Slide}
-                        message={snackbarMessage}
-                        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-                        style={{ marginBottom: '60px' }}
-                    />
+                <IconButton
+                    aria-label="close"
+                    onClick={handleCloseClick}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+                <div className="restaurant-details__restaurant">
+                    <h2 className="restaurant-details__name">{restaurantDetails.name}</h2>
+                    <div className="restaurant-details__add">
+                        <IconButton disableTouchRipple className="restaurant-details__add-icon" size="medium" onClick={() => addItemHandler(placeId)} style={{ color:'#73649b' }}>
+                            <AddIcon fontSize="inherit"/>
+                        </IconButton>
+                        <Snackbar
+                            open={snackbarOpen}
+                            autoHideDuration={2000} 
+                            onClose={() => setSnackbarOpen(false)}
+                            TransitionComponent={Slide}
+                            message={snackbarMessage}
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                            style={{ marginBottom: '60px' }}
+                        />
+                    </div>
                 </div>
-            </div>
-            <p className="restaurant-details__info"><strong>Rating: </strong>{restaurantDetails.rating}</p>
-            <p className="restaurant-details__info"><strong>Address: </strong>{restaurantDetails.address}</p>
-            <p className="restaurant-details__info"><strong>Phone: </strong>{restaurantDetails.phone}</p>
-            <p className="restaurant-details__info"><strong>Website: </strong>{restaurantDetails.website}</p>
-            <p className="restaurant-details__hours"><strong>Hours: </strong></p>
+                
+                <p className="restaurant-details__info"><strong>Rating: </strong>{restaurantDetails.rating}</p>
+                <p className="restaurant-details__info"><strong>Price Level: </strong>{restaurantDetails.priceLevel}</p>
+
+                {/* Display the photos */}
+                <div className="restaurant-details__photo-container">
+                    <div className="restaurant-details__photos">
+                        {restaurantDetails.photos.map((photo, index) => (
+                            <img
+                                key={index}
+                                src={photo.photo_url}
+                                alt={`${restaurantDetails.name} ${index + 1}`}
+                                className="restaurant-details__photo"
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <p className="restaurant-details__info"><strong>Address: </strong>{restaurantDetails.address}</p>
+                <p className="restaurant-details__info"><strong>Phone: </strong>{restaurantDetails.phone}</p>
+                <p className="restaurant-details__info"><strong>Website: </strong>{restaurantDetails.website}</p>
+                <p className="restaurant-details__hours"><strong>Hours: </strong></p>
+                    <ul className="restaurant-details__list">
+                        {restaurantDetails.hours.map((hour, index) => (
+                            <li key={index} className="restaurant-details__hour">{hour}</li>
+                        ))}
+                    </ul>
+                <p className="restaurant-details__reviews"><strong>Reviews ({restaurantDetails.reviews.length}):</strong></p>
                 <ul className="restaurant-details__list">
-                    {restaurantDetails.hours.map((hour, index) => (
-                        <li key={index} className="restaurant-details__hours">{hour}</li>
+                    {restaurantDetails.reviews.map((review, index) => (
+                        <li key={index} className="restaurant-details__review">
+                            <p className="restaurant-details__review-name">{review.author_name}</p>
+                            <div className="restaurant-details__review-details">
+                                <p className="restaurant-details__review-rating">Rating: {review.rating}</p>
+                                <p className="restaurant-details__review-time">{review.relative_time_description}</p>
+                            </div>
+                            <p className="restaurant-details__info">{review.text}</p>
+                        </li>
                     ))}
                 </ul>
+                
+
+                {/* <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {restaurantDetails.photos.map((photo, index) => (
+                        <ImageListItem key={index}>
+                            <img
+                                srcSet={`${photo.photo_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${photo.photo_url}?w=164&h=164&fit=crop&auto=format`}
+                                alt={`${restaurantDetails.name} ${index + 1}`}
+                                loading="lazy"
+                            />
+                        </ImageListItem>
+                    ))}
+                </ImageList> */}
             </div>
         </div>
     );
